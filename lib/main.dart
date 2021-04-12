e3a0aa6b74ac9aa4522b65b5ae3ea1562b2b3ab9
Sadhana <73754194+SadhanaParameswaran@users.noreply.github.com>
2021-04-12 16:04:01 +0530

    package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'ToDoList.dart';
-import 'package:flutter/cupertino.dart';
 import 'BottomNavigation.dart';
 import 'radioButton.dart';
 
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BottomNagivation(),
+      home: BottomNavigation(),
     );
   }
 }
